dd8bf5bda4d1e8b5b6593c94e2ae11443f70b859
Kutlay Özger <kutlayozger@gmail.com>
2013-07-11 23:50:11 +0300

    first commit

---
+++ assets/www/js/screens/play.js
@@ -0,0 +1,18 @@
+
+game.PlayScreen = me.ScreenObject.extend({
+ 
+    onResetEvent: function() {
+        // stuff to reset on state change
+        // load a level
+        me.levelDirector.loadLevel("area01");
+    },
+ 
+    /* ---
+ 
+    action to perform when game is finished (state change)
+ 
+    --- */
+    onDestroyEvent: function() {
+    }
+ 
+});
